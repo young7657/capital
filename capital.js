ac5a2c1960764d21acc862f53368bf88eef38088
youngliu <young129@126.com>
2015-05-21 22:00:07 +0800

    init function

---
+++ capital.js
@@ -0,0 +1,11 @@
+/**依赖jquery*/
+(function($) {
+    $.capital = function(option) {
+        var numArr = ['一', '二', '三', '四', '五', '六', '七', '八', '九']；
+	var unitArr = ['十'， '百', '千', '万', '亿'];
+	var result = '';
+	
+	return result;
+    };
+
+})(jQuery);
