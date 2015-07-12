3c1d060227a5113f71c9c442bd7a7ba839deb810
youngliu <youngliu129@126.com>
2015-07-12 17:07:58 +0800

    处理零

--- zh_num.js
+++ zh_num.js
@@ -2,20 +2,29 @@
 (function($) {
     var CNUM = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
     var UNIT = ['', '十', '百', '千', '万', '亿'];
-        var prepare = {'20' : '二十','30' : '三十', '40' : '四十','50' : '五十','60' : '六十','70' : '七十','80' : '八十','90' : '九十','100' : '一百'};
+
     $.zh_num = function(num) {
+        var result = '';
         //数字翻转遍历时可以适配对应单位
         num = (+num).toString().split('').reverse();
 
         var l = num.length;
         var pre = '';
-        var result = '';
         for(var i = l - 1; i >= 0; i--) {
             var unit = UNIT[i];
-            var n = CNUM[num[i]];
+            var n = +num[i];
+            var cn = CNUM[n];
+            //判断如果为0则缓存零值，直到遇到下一位不是零的数
+            if (n === 0) {
+                pre = cn;
+                continue;
+            }
 
-            result += n + unit;
+            result += pre + cn + unit;
+            //缓存的零只能用一次
+            pre = '';
         }
+        console.log(result);
     };
 
 })(jQuery);
