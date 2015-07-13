6f41b68378d2971e1d68f0a2e26afcf375835520
youngliu <youngliu129@126.com>
2015-07-13 18:00:29 +0800

    处理多少零多少的情况，例如一万零一

--- zh_num.js
+++ zh_num.js
@@ -2,14 +2,19 @@
 (function($) {
     var CNUM = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
     var UNIT = ['', '十', '百', '千', '万', '亿'];
+    var EXUNIT = ['', '万', '亿'];
 
-    $.zh_num = function(num) {
-        var result = '';
-        //数字翻转遍历时可以适配对应单位
-        num = (+num || 0).toString().split('').reverse();
-
+    function getNum(N, cheng, weishu) {
+      console.log(N);
+      console.log(cheng);
+      console.log(weishu);
+        var num = N.splice(0, 4);
         var l = num.length;
         var pre = '';
+        var result = '';
+        var cc = cheng + 1;
+        if (cc >= 3) cc = 0;
+
         for(var i = l - 1; i >= 0; i--) {
             var unit = UNIT[i];
             var n = +num[i];
@@ -23,6 +28,27 @@
             //缓存的零只能用一次
             pre = '';
         }
+        var child = '';
+        if (N.length) {
+            child = getNum(N, cc, num);
+        }
+        var exunit =  EXUNIT[cheng];
+        if (result == '') {
+            exunit = '';
+        }
+        result = result + exunit;
+        if (!result && +weishu.join('')) {
+            result = '零';
+        }
+        return child + result;
+    }
+
+    $.zh_num = function(num) {
+        var result = '';
+        //数字翻转遍历时可以适配对应单位
+        num = (+num || 0).toString().split('').reverse();
+
+        var result = getNum(num, 0, [0]);
         console.log(result);
     };
 
